0536e4e842421364c03246d354f26c99d72906a5
aelkheir <90580077+aelkheir@users.noreply.github.com>
2024-09-08 14:20:53 +0300

    Prevent autho complete from replacing.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -75,9 +75,6 @@ return {
                 lua_ls = {
                     settings = {
                         Lua = {
-                            completion = {
-                                callSnippet = 'Replace',
-                            },
                             diagnostics = { disable = { 'missing-fields' } },
                         },
                     },
