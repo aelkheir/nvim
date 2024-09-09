bafc3a36bf4cdb85ddb6cdd8257e3300c2b426f4
aelkheir <90580077+aelkheir@users.noreply.github.com>
2024-09-10 00:43:42 +0300

    Remove lualine line separators

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,8 @@ return {
         require('lualine').setup {
             options = {
                 theme = 'tokyonight',
+                section_separators = '',
+                component_separators = '',
             },
         }
     end,
