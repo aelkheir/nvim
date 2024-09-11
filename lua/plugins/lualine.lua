512d46ba7b21e3772253c16d057f5272cfff45d2
aelkheir <90580077+aelkheir@users.noreply.github.com>
2024-09-11 04:52:59 +0300

    Fix lualine git colors contrast.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,11 +2,24 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local colors = require('tokyonight.colors').setup()
         require('lualine').setup {
             options = {
                 theme = 'tokyonight',
                 section_separators = '',
                 component_separators = '',
+            },
+            sections = {
+                lualine_b = {
+                    {
+                        'diff',
+                        diff_color = {
+                            added = { fg = colors.git.add },
+                            modified = { fg = colors.git.change },
+                            removed = { fg = colors.git.delete },
+                        },
+                    },
+                },
             },
         }
     end,
